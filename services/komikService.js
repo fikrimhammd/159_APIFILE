7d4c5f793435d1f34ce9b204f0f575dfd0e8dd74
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-25 14:59:24 +0700

    validasi data

--- services/komikService.js
+++ services/komikService.js
@@ -1,2 +1,11 @@
 // KomikService.js
 
+async function createKomik(database, komikData) {
+  const { title, description, author, imageType, imageName, imageData } = komikData;
+
+  // Validasi data wajib
+  if (!title || !description || !author) {
+    throw new Error('Title, description, dan author wajib diisi');
+  }
+
+ 
